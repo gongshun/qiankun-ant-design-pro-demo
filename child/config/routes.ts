d8d77d6feb55654925cd6314b10db15b1f235298
gongshun <2440606146@qq.com>
2021-10-28 15:29:46 +0800

    feat: 增加文字区分主应用和子应用

--- child/config/routes.ts
+++ child/config/routes.ts
@@ -7,7 +7,7 @@ export default [
       { component: './404' },
     ],
   },
-  { path: '/welcome', name: '欢迎', icon: 'smile', component: './Welcome' },
+  { path: '/welcome', name: '子应用的欢迎', icon: 'smile', component: './Welcome' },
   {
     path: '/admin',
     name: '管理页',
@@ -15,11 +15,11 @@ export default [
     access: 'canAdmin',
     component: './Admin',
     routes: [
-      { path: '/admin/sub-page', name: '二级管理页', icon: 'smile', component: './Welcome' },
+      { path: '/admin/sub-page', name: '子应用的二级管理页', icon: 'smile', component: './Welcome' },
       { component: './404' },
     ],
   },
-  { name: '查询表格', icon: 'table', path: '/list', component: './TableList' },
+  { name: '子应用的查询表格', icon: 'table', path: '/list', component: './TableList' },
   { path: '/', redirect: '/welcome' },
   { component: './404' },
 ];
